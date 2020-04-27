0c4bc5611fc988133ca51e293779de8bd13d6582
loikein <wanleiqiong@gmail.com>
2020-04-27 17:44:49 +0200

    Update clumsy-update.js

--- static/js/clumsy-update.js
+++ static/js/clumsy-update.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	function insertAfter(el, referenceNode) {referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);}
 	var githubAPI = "https://api.github.com/repos/loikein/loikein.github.io/commits?page=1&per_page=1";
-	var lastUpdateTitle = document.getElementById("last-update").getElementsByClassName("section")[0];
+	var lastUpdateTitle = document.getElementById("last-update").children[0];
 	$.get(githubAPI, function(commit) {
 		lastUpdateTitle.append( ': ' + ($.timeago(commit[0].commit.committer.date)));
 		var updateMes = document.createElement('div');
