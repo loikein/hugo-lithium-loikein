e91c8ccf369243cb182337cccd5fbddcec739970
loikein <wanleiqiong@gmail.com>
2020-10-31 20:04:12 +0100

    remove custom static files

--- static/js/scroll-toc.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    let root = document.documentElement;
-    root.style.setProperty("--scrolltop", root.scrollTop);
-
-    document.addEventListener("scroll", evt => {
-        root.style.setProperty("--scrolltop", root.scrollTop);
-    });
-});
